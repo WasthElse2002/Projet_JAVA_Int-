97be6054e42a5a3c0f59f4ce8796a1b1fefd2582
stanislasbdx <s.castaybert@deal.fr>
2022-11-28 14:16:27 +0100

    Ajout d'une enum Salad pour la classe Burger

---
+++ bpj/src/Salad.java
@@ -0,0 +1,18 @@
+public enum Salad {
+	LETUCE("Laitue"),
+	BATAVIA("Batavia"),
+	SCAROL("Scarol"),
+	ARAGULA("Roquette");
+
+	private final String type;
+
+	public String getType()
+	{
+		return this.type;
+	}
+
+	Salad(String type)
+	{
+		this.type = type;
+	}
+}
